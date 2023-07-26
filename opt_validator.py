fc9893816e6b74ca800c1608f55634e608c5b396
Your Name <you@example.com>
2023-07-26 22:21:33 +1000

    aider: Refactored functionality to search the root folder for `.opt` files if no file name parameter was passed in, and loop through them to validate each one.
    # Aider chat conversation:
    
    USER: refactor functionality so if no file name parameter passed in, look in the root folder for any `.opt` files and if found loop through them.
    
    ASSISTANT: To implement this functionality, we need to make changes to the `opt_validator.py` file.
    
    Here are the steps we will follow:
    
    1. Import the `os` module to interact with the file system.
    2. Modify the `opt_file` argument to be optional.
    3. If no `opt_file` argument is provided, search the root directory for `.opt` files.
    4. If `.opt` files are found, loop through them and validate each one.
    
    Here is the code change for `opt_validator.py`:
    
    ```python
    opt_validator.py
    <<<<<<< ORIGINAL
    import argparse
    import subprocess
    
    def main():
        # Create a parser object
        parser = argparse.ArgumentParser(description='Validate .opt file with OperationalTemplateExtra.xsd schema using xmllint.')
    
        # Add an argument for the .opt file
        parser.add_argument('opt_file', type=str, help='The .opt file to validate.')
    
        # Parse the arguments
        args = parser.parse_args()
    
        # Run the xmllint command
        command = f'xmllint --noout --schema OperationalTemplateExtra.xsd {args.opt_file}'
        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
        stdout, stderr = process.communicate()
    
        # Print the output of the xmllint command
        if process.returncode != 0:
            print(f'Error validating {args.opt_file}:\n{stderr.decode()}')
        else:
            print(f'Successfully validated {args.opt_file}.')
    
    if __name__ == '__main__':
        main()
    =======
    import argparse
    import subprocess
    import os
    
    def main():
        # Create a parser object
        parser = argparse.ArgumentParser(description='Validate .opt file with OperationalTemplateExtra.xsd schema using xmllint.')
    
        # Add an argument for the .opt file
        parser.add_argument('opt_file', type=str, nargs='?', help='The .opt file to validate.')
    
        # Parse the arguments
        args = parser.parse_args()
    
        # If no opt_file argument is provided, search the root directory for .opt files
        if args.opt_file is None:
            opt_files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.opt')]
        else:
            opt_files = [args.opt_file]
    
        # Loop through the .opt files and validate each one
        for opt_file in opt_files:
            # Run the xmllint command
            command = f'xmllint --noout --schema OperationalTemplateExtra.xsd {opt_file}'
            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
            stdout, stderr = process.communicate()
    
            # Print the output of the xmllint command
            if process.returncode != 0:
                print(f'Error validating {opt_file}:\n{stderr.decode()}')
            else:
                print(f'Successfully validated {opt_file}.')
    
    if __name__ == '__main__':
        main()
    >>>>>>> UPDATED
    ```
    
    This code will now search the root directory for `.opt` files if no `opt_file` argument is provided. It will then validate each `.opt` file found.

--- opt_validator.py
+++ opt_validator.py
@@ -1,26 +1,35 @@
 import argparse
 import subprocess
+import os
 
 def main():
     # Create a parser object
     parser = argparse.ArgumentParser(description='Validate .opt file with OperationalTemplateExtra.xsd schema using xmllint.')
     
     # Add an argument for the .opt file
-    parser.add_argument('opt_file', type=str, help='The .opt file to validate.')
+    parser.add_argument('opt_file', type=str, nargs='?', help='The .opt file to validate.')
     
     # Parse the arguments
     args = parser.parse_args()
     
-    # Run the xmllint command
-    command = f'xmllint --noout --schema OperationalTemplateExtra.xsd {args.opt_file}'
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    
-    # Print the output of the xmllint command
-    if process.returncode != 0:
-        print(f'Error validating {args.opt_file}:\n{stderr.decode()}')
+    # If no opt_file argument is provided, search the root directory for .opt files
+    if args.opt_file is None:
+        opt_files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.opt')]
     else:
-        print(f'Successfully validated {args.opt_file}.')
+        opt_files = [args.opt_file]
+    
+    # Loop through the .opt files and validate each one
+    for opt_file in opt_files:
+        # Run the xmllint command
+        command = f'xmllint --noout --schema OperationalTemplateExtra.xsd {opt_file}'
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        
+        # Print the output of the xmllint command
+        if process.returncode != 0:
+            print(f'Error validating {opt_file}:\n{stderr.decode()}')
+        else:
+            print(f'Successfully validated {opt_file}.')
 
 if __name__ == '__main__':
     main()
